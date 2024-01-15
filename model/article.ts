0a00742cb52eb5172c4c6638d109e73a7b86ec7b
Vamshikiran Morlawar <vamshimorlawar@gmail.com>
2024-01-15 12:35:00 +0530

    Article Model added

---
+++ model/article.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, model } from "mongoose";
+
+interface ArticleDocument extends Document {
+  createdBy: String;
+  creatorId: mongoose.Schema.Types.ObjectId;
+  title: String;
+  tagline: String;
+  category: String;
+  editCount: Number;
+}
+
+const ArticleSchema = new mongoose.Schema(
+  {
+    createdBy: { type: String },
+    creatorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    title: { type: String },
+    tagline: { type: String },
+    category: { type: String },
+    editCount: { type: Number },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Article ||
+  model<ArticleDocument>("Article", ArticleSchema);
