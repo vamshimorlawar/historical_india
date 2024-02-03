d77a60c5dcae65bb12c8915e1e4278ff56b58124
Vamshikiran <vamshimorlawar@gmail.com>
2024-02-03 15:50:42 +0530

    uifix: nav and menu for smaller screens

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -4,10 +4,10 @@ const HeroBanner = () => {
   return (
     <div>
       <div className="flex items-center justify-center flex-col w-100 h-[500px]">
-        <div className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] hidden md:block">
+        <div className="text-center text-xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] md:block">
           Welcome to Historical India !
         </div>
-        <span className="max-w-[700px] text-center text-lg text-muted-foreground mt-2 sm:text-xl">
+        <span className="max-w-[500px] text-center text-md text-muted-foreground mt-2 md:text-xl">
           Contribute to open source community project and help create
           encyclopedia of the Indian history.
         </span>
